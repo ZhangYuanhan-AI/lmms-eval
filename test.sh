0e841b8b6e103a329490b9ffb181eee47b314128
john.zhang <john.zhang@bytedance.com>
2025-02-18 01:55:26 +0000

    update

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-# accelerate launch --main_process_port=12345 --num_processes=1 \
-# -m lmms_eval \
-# --model openai_client \
-# --model_args model=gpt-4o-2024-05-13,max_frames=120,fps=5 \
-# --tasks hardvideo_single_mc \
-# --batch_size 1 \
-# --log_samples \
-# --log_samples_suffix gpt4o \
-# --output_path ./logs/
+accelerate launch --main_process_port=12345 --num_processes=1 \
+-m lmms_eval \
+--model openai_client \
+--model_args model=gpt-4o-2024-05-13,max_frames=120,fps=1 \
+--tasks hardvideo_single_mc \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix gpt4o \
+--output_path ./logs/
 
 
 
@@ -21,12 +21,12 @@
 # --output_path ./logs/
 
 
-accelerate launch --main_process_port=12345 --num_processes=1 \
--m lmms_eval \
---model openai_client \
---model_args model=llava_next_video_7b,max_frames=64,fps=1 \
---tasks hardvideo_single_mc \
---batch_size 1 \
---log_samples \
---log_samples_suffix llava_next_video_7b_api \
---output_path ./logs/
+# accelerate launch --main_process_port=12345 --num_processes=1 \
+# -m lmms_eval \
+# --model openai_client \
+# --model_args model=llava_next_video_7b,max_frames=64,fps=1 \
+# --tasks hardvideo_single_mc \
+# --batch_size 1 \
+# --log_samples \
+# --log_samples_suffix llava_next_video_7b_api \
+# --output_path ./logs/
